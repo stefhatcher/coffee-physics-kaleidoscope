26aa923b5ce92dc4625376a587f2d1baa59c407e
stefhatcher <stefhatcher@gmail.com>
2012-10-11 20:15:28 -0700

    checking in all of the things

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function( grunt ) {
       dist: {
         // http://compass-style.org/help/tutorials/configuration-reference/#configuration-properties
         options: {
-          css_dir: 'temp/styles',
+          css_dir: 'app/styles',
           sass_dir: 'app/styles',
           images_dir: 'app/images',
           javascripts_dir: 'temp/scripts',
@@ -61,10 +61,10 @@ module.exports = function( grunt ) {
       },
       reload: {
         files: [
-          'app/*.html',
-          'app/styles/**/*.css',
-          'app/scripts/**/*.js',
-          'app/images/**/*'
+          '*/*.html',
+          '*/styles/**/*.css',
+          '*/scripts/**/*.js',
+          '*/images/**/*'
         ],
         tasks: 'reload'
       }
